bc4a53741294ea4dab09c703039a14c21c356c32
Maggie McDowell <maggiemcdowell4@gmail.com>
2024-03-10 13:45:11 -0600

    <created loop in js>

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,30 @@
+const topic = "HTML";
+
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+    console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+} else {
+    console.log('Please try again!');
+}
+const topics = ['HTML', 'CSS', 'Git', 'JaveScript'];
+for(let x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+/*
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+    console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+} else {
+    console.log('Please try again!');
+}
+*/
